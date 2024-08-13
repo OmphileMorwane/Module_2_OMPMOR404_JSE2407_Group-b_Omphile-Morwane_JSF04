cf6d7590cabdee1d0e62c6d603566096b3bc2fa9
Omphile Morwane <omphilemorwane@gmail.com>
2024-08-13 10:56:02 +0200

    Added a theme button and updated the store

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -38,6 +38,22 @@
           :product="product"
         />
       </div>
+
+      <div :class="theme">
+        <button
+          @click="toggleTheme"
+          style="
+            position: relative;
+            z-index: 9999;
+            background-color: red;
+            color: white;
+          "
+        >
+          {{
+            theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"
+          }}
+        </button>
+      </div>
     </div>
   </div>
 
@@ -83,6 +99,8 @@ export default {
       () => store.getters.filteredAndSortedProducts
     );
 
+    const theme = computed(() => store.state.theme);
+
     /**
      * Filters the products by the selected category.
      */
@@ -92,6 +110,13 @@ export default {
 
     const sortProducts = () => {
       // No need to update the state here as v-model does that
+    };
+
+    /**
+     * Toggles between light and dark mode.
+     */
+    const toggleTheme = () => {
+      store.commit("toggleTheme");
     };
 
     /**
@@ -110,6 +135,8 @@ export default {
       filteredAndSortedProducts,
       filterByCategory,
       sortProducts,
+      theme,
+      toggleTheme,
     };
   },
 };
