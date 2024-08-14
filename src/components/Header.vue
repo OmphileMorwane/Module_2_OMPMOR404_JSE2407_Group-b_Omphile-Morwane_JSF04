5d2377b56b5da973c8f12f9e441e94e0818146ba
Omphile Morwane <omphilemorwane@gmail.com>
2024-08-14 09:56:09 +0200

    Changed the a tags to to Router links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,14 +2,14 @@
   <header class="header">
     <nav class="nav">
       <div class="nav-container">
-        <a href="/" class="logo">
+        <RouterLink to="/" class="logo">
           <img
             src="/src/assets/online-shop.png"
             class="logo-img"
             alt="Flowbite Logo"
           />
           <span class="logo-text">SwiftCart</span>
-        </a>
+        </RouterLink>
         <button
           @click="toggleNavbar"
           type="button"
@@ -40,10 +40,10 @@
         >
           <ul class="navbar-list">
             <li>
-              <a href="#" class="navbar-item">Wishlist</a>
+              <RouterLink to="/wishlist" class="navbar-item">Wishlist</RouterLink>
             </li>
             <li class="navbar-cart">
-              <a href="#" class="navbar-item">
+              <RouterLink to="/cart" class="navbar-item">
                 <div class="cart-notification">
                   <p class="cart-count">2</p>
                 </div>
@@ -61,13 +61,13 @@
                     d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
                   />
                 </svg>
-              </a>
+              </RouterLink>
             </li>
             <li>
-              <a href="#" class="navbar-item cart-mobile">Cart</a>
+              <RouterLink to="/cart" class="navbar-item cart-mobile">Cart</RouterLink>
             </li>
             <li>
-              <a href="login" class="navbar-item">Login</a>
+              <RouterLink to="/login" class="navbar-item">Login</RouterLink>
             </li>
           </ul>
         </div>
@@ -78,6 +78,7 @@
 
 <script setup>
 import { ref } from 'vue';
+import { RouterLink } from 'vue-router'
 
 const navbarOpen = ref(false);
 
