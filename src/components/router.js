5bbfa7fce9d6f42d8cdcb81a6ad851b02a48ee96
Omphile Morwane <omphilemorwane@gmail.com>
2024-08-19 13:10:15 +0200

    Added cart routing and imported store

--- src/components/router.js
+++ src/components/router.js
@@ -1,7 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import ProductList from '../components/ProductList.vue';
-import ProductDetail from '../components/ProductDetail.vue';
-import Login from '../components/Login.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import store from "../components/store"; 
+import ProductList from "../components/ProductList.vue";
+import ProductDetail from "../components/ProductDetail.vue";
+import Login from "../components/Login.vue";
+import Cart from "./cart.vue";
 
 /**
  * Array of route definitions.
@@ -9,24 +11,34 @@ import Login from '../components/Login.vue';
  */
 const routes = [
   {
-    path: '/',
-    name: 'ProductList',
+    path: "/",
+    name: "ProductList",
     component: ProductList,
   },
   {
-    path: '/product/:id',
-    name: 'ProductDetail',
+    path: "/product/:id",
+    name: "ProductDetail",
     component: ProductDetail,
     props: true, // Allows passing route params as props
   },
-  
   {
-    path: '/login',
-    name: 'Login',
+    path: "/login",
+    name: "Login",
     component: Login,
   },
+  {
+    path: "/cart",
+    name: "Cart",
+    component: Cart,
+    beforeEnter: (to, from, next) => {
+      if (!store.state.isAuthenticated) {
+        next({ name: "Login", query: { redirect: to.fullPath } });
+      } else {
+        next();
+      }
+    },
+  },
 ];
-
 
 /**
  * Creates a new Vue Router instance.
