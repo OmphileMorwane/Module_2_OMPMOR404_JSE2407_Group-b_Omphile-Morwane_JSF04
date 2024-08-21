82416855ca6686b7925c33f38fa7042a76634f18
Omphile Morwane <omphilemorwane@gmail.com>
2024-08-21 09:49:40 +0200

    Handled auth and cart initiation

--- src/main.js
+++ src/main.js
@@ -1,30 +1,35 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './components/router'
-import store from './components/store'
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
-
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./components/router";
+import store from "./components/store";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 /**
  * Creates and mounts the Vue application.
  */
-const app = createApp(App)
+const app = createApp(App);
+
+// Restore cart from localStorage if it exists
+const savedCart = JSON.parse(localStorage.getItem("cart"));
+if (savedCart) {
+  savedCart.forEach((item) => store.dispatch("addToCart", item));
+}
 
 /**
  * Uses the Vue Router for the application.
  */
 app.use(router);
 
-
 /**
  * Uses the Vue store for the application.
  */
 app.use(store);
 
-
 /**
- * Mounts the Vue application to the DOM element with the id 'app'.
+ * Dispatches the initializeAuth action to handle authentication state and cart initialization.
  */
-app.mount('#app');
+store.dispatch("initializeAuth").then(() => {
+  // Mount the Vue application to the DOM element with the id 'app'
+  app.mount("#app");
+});
