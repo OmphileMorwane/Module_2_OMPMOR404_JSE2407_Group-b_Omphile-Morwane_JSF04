640e4d38e89b1af86783fbb61462aa5c04836662
Omphile Morwane <omphilemorwane@gmail.com>
2024-08-21 15:12:44 +0200

    route protection, added wishlist and comparison routes

--- src/components/router.js
+++ src/components/router.js
@@ -4,42 +4,63 @@ import ProductList from "../components/ProductList.vue";
 import ProductDetail from "../components/ProductDetail.vue";
 import Login from "../components/Login.vue";
 import Cart from "./cart.vue";
+import ComparisonPage from "./comparison.vue";
+import Wishlist from "./Wishlist.vue";
 
-/**
- * Array of route definitions.
- * @type {Array<Object>}
- */
 const routes = [
   {
-    path: "/",
-    name: "ProductList",
+    path: '/',
+    name: 'ProductList',
     component: ProductList,
   },
   {
-    path: "/product/:id",
-    name: "ProductDetail",
+    path: '/product/:id',
+    name: 'ProductDetail',
     component: ProductDetail,
     props: true, // Allows passing route params as props
   },
   {
-    path: "/login",
-    name: "Login",
+    path: '/login',
+    name: 'Login',
     component: Login,
   },
   {
-    path: "/cart",
-    name: "Cart",
+    path: '/cart',
+    name: 'Cart',
     component: Cart,
     beforeEnter: (to, from, next) => {
       if (!store.state.isAuthenticated) {
-        next({ name: "Login", query: { redirect: to.fullPath } });
+        next({ name: 'Login', query: { redirect: to.fullPath } });
+      } else {
+        next();
+      }
+    },
+  },
+  {
+    path: '/comparison',
+    name: 'ComparisonPage',
+    component: ComparisonPage,
+    beforeEnter: (to, from, next) => {
+      if (!store.state.isAuthenticated) {
+        next({ name: 'Login', query: { redirect: to.fullPath } });
+      } else {
+        next();
+      }
+    },
+  },
+  {
+    path: '/wishlist',
+    name: 'Wishlist',
+    component: Wishlist,
+    beforeEnter: (to, from, next) => {
+      if (!store.state.isAuthenticated) {
+        next({ name: 'Login', query: { redirect: to.fullPath } });
       } else {
         next();
       }
     },
   },
 ];
-
 /**
  * Creates a new Vue Router instance.
  * @returns {Router} The Vue Router instance.
@@ -47,6 +68,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  // Global navigation guard to handle route protection
+  const isAuthenticated = store.state.isAuthenticated;
+  const requiresAuth = to.meta.requiresAuth;
+
+  if (requiresAuth && !isAuthenticated) {
+    next({ name: 'Login', query: { redirect: to.fullPath } });
+  } else {
+    next();
+  }
 });
 
 export default router;
